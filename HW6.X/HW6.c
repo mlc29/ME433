81bea23bb92356bfbcfa9dbe23f8747f3b101e12
mlc29 <marcuscisco2023@u.northwestern.edu>
2023-05-02 18:05:15 -0500

    HW6.2
    Updated with generic i2c write function

--- HW6.X/HW6.c
+++ HW6.X/HW6.c
@@ -2,9 +2,14 @@
 #include "nu32dip.h"
 #include "i2c_master_noint.h"
 
-unsigned char addy = 0b01000000;
+unsigned char addy_write = 0b01000000;
+unsigned char addy_read = 0b01000001;
+unsigned char IODIR = 0x00;
+unsigned char GPIO = 0x09;
+unsigned char OLAT = 0x0A;
+
 void delay();
-void set_GP7(unsigned int x);
+void generic_i2c_write(unsigned char address, unsigned char SFR, unsigned char value);
 
 int main(void) {
     
@@ -14,11 +19,7 @@ int main(void) {
     i2c_master_setup();
     
     // Init the chip, GP0 as input, GP7 as output
-    i2c_master_start();
-    i2c_master_send(addy);
-    i2c_master_send(0x00);
-    i2c_master_send(0b00000001);
-    i2c_master_stop();
+    generic_i2c_write(addy_write, IODIR, 0b00000001);
     
     while(1){
         // Blink the yellow LED for heartbeat
@@ -26,15 +27,17 @@ int main(void) {
         delay();
         NU32DIP_YELLOW = 1;
         delay();
+        NU32DIP_YELLOW = 0;
         
         // Blink GP7 to check for function
-        set_GP7(1);
+        generic_i2c_write(addy_write, OLAT, 0b10000000);
         delay();
-        set_GP7(0);
+        generic_i2c_write(addy_write, OLAT, 0b00000000);
         delay();
         
     }
 }
+
 
 // delay function for blinking LEDs
 void delay(void){
@@ -42,13 +45,12 @@ void delay(void){
     while(_CP0_GET_COUNT()<12000000){}
 }
 
-// function for blinking GP7
-void set_GP7(unsigned int x){
-    unsigned short i;
-    i = x<<7;
+
+// generic i2c write function that writes to any i2c chip
+void generic_i2c_write(unsigned char address, unsigned char SFR, unsigned char value){
     i2c_master_start();
-    i2c_master_send(addy);
-    i2c_master_send(0x0A);
-    i2c_master_send(i);
+    i2c_master_send(address);
+    i2c_master_send(SFR);
+    i2c_master_send(value);
     i2c_master_stop();
 }
