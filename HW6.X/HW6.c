60bb073c6d49cd7d7b45c0e936f5e83ea8724b10
mlc29 <marcuscisco2023@u.northwestern.edu>
2023-05-02 14:49:01 -0500

    HW6.1
    Started HW6 with I2C. Got the PIC led heartbeat going and able to blink GP7 output.

--- HW6.X/HW6.c
+++ HW6.X/HW6.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
+#include "nu32dip.h"
 #include "i2c_master_noint.h"
+
+unsigned char addy = 0b01000000;
+void delay();
+void set_GP7(unsigned int x);
 
 int main(void) {
     
     NU32DIP_Startup();
+    
+    // Init I2C pins B8(SCL) and B9(SDA)
     i2c_master_setup();
     
+    // Init the chip, GP0 as input, GP7 as output
+    i2c_master_start();
+    i2c_master_send(addy);
+    i2c_master_send(0x00);
+    i2c_master_send(0b00000001);
+    i2c_master_stop();
+    
     while(1){
+        // Blink the yellow LED for heartbeat
+        NU32DIP_YELLOW = 0;
+        delay();
+        NU32DIP_YELLOW = 1;
+        delay();
+        
+        // Blink GP7 to check for function
+        set_GP7(1);
+        delay();
+        set_GP7(0);
+        delay();
         
     }
+}
+
+// delay function for blinking LEDs
+void delay(void){
+    _CP0_SET_COUNT(0);
+    while(_CP0_GET_COUNT()<12000000){}
+}
+
+// function for blinking GP7
+void set_GP7(unsigned int x){
+    unsigned short i;
+    i = x<<7;
+    i2c_master_start();
+    i2c_master_send(addy);
+    i2c_master_send(0x0A);
+    i2c_master_send(i);
+    i2c_master_stop();
 }
---
+++ HW6.X/build/default/production/HW6.o.d
@@ -0,0 +1,5 @@
+build/default/production/HW6.o: HW6.c nu32dip.h i2c_master_noint.h
+
+nu32dip.h:
+
+i2c_master_noint.h:
